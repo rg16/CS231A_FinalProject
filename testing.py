c8e58438f1e5a4c1b296b3b690f1f2b7c90df719
Andre Cornman <andrecornman@DN0a1fc46e.SUNet>
2018-02-26 16:07:47 -0800

    push testing file

--- testing.py
+++ testing.py
@@ -2,15 +2,21 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture('testVid.avi')
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('testOutput.avi',fourcc, 20, (1920, 1080, 3))
+#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
 while(cap.isOpened()):
     ret, frame = cap.read()
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     print ret
     if ret:
-    	cv2.imshow('frame',frame)
+    	out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
+out.release()
 cv2.destroyAllWindows()
 
 """
